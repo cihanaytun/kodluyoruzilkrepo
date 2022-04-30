e5868e2f8a856d9b7698b5ed1b3b162cd0871257
cihanaytun <aytuncihan@gmail.com>
2022-04-30 23:51:11 +0300

    two repos merged

---
+++ c#_101/console_programlama/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wrold");
+            
+            Console.WriteLine("İsminizi girin ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+ name +" " + surname);
+
+
+
+        }
+    }
+}
+
+
