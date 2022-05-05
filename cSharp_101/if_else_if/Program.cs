eb8372cc685b4991938ec3dcba2b9c6bd2df452f
cihanaytun <aytuncihan@gmail.com>
2022-05-05 21:14:42 +0300

    if_else_ternaryIf

---
+++ cSharp_101/if_else_if/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Gunaydin!" + " " + time);               
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("Iyi Gunler!" + " " + time);
+            }
+            else
+            {
+                Console.WriteLine("Iyi Geceler!" + " " + time);
+            }
+
+
+            //Ternary If
+
+            string sonuc = time <= 18 ? "Iyi Gunler!" : "Iyi Geceler!";
+            sonuc = time >= 6 && time < 11 ? "Gunaydin!" : time <= 18 ? "Iyi Gunler!" :"Iyi Geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
