01777b90641d2f4f640b05b028bda4b1fe2f5d73
cihanaytun <aytuncihan@gmail.com>
2022-06-03 20:11:03 +0300

    class_6

---
+++ cSharp_101/classes/class_5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace class_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Struct(Yapı) Kavramı  
+
+            //class  alan hesabı
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+            Console.WriteLine("Class Alan Hesabı :{0}",dikdortgen.AlanHesapla()); 
+            
+            
+            //class  alan hesabı
+            //Dikdortgen_Struct dikdortgen_struct; Böylede tanımlanabilir
+            Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct();
+            dikdortgen_struct.KisaKenar = 3;
+            dikdortgen_struct.UzunKenar = 4;
+            Console.WriteLine("Struct Alan Hesabı :{0}",dikdortgen.AlanHesapla()); 
+        }
+    }
+
+
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+
+    //Struct
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }       
+    }
+}
---
+++ cSharp_101/classes/class_6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace class_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enumlar
+            Console.WriteLine(Gunler.Pazar);//string değerini veriyor
+            Console.WriteLine((int)Gunler.Cumartesi);//indis değerini yada kendi vermiş olduğumuz değeri getiriyor 
+
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.CokSicak)
+            {
+                Console.WriteLine("Hava çok sıcak");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Hava sıcak");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSicak)
+            {
+                Console.WriteLine("Hava iyi");                
+            }
+        }
+    }
+
+    // indis numaraları 0 dan başlar,sıralıdır
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+
+    }
+}
